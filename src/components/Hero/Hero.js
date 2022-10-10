4ef841372521a386847198f36f0b7885f9f2ae78
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-11 00:02:12 +0600

    home page successfully done

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Hero.css'
+import hero from '../../assets/images/hero.png'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className="hero-content">
+              <h2 className='title2'>Take A <span className='violet'>Challange</span> </h2>
+              <h1 className='title1'>Justify Yourself</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum reiciendis laborum nostrum dolorem aut veniam consequatur delectus tempora reprehenderit maxime.</p>
+             <div className='btnDiv'>
+             <button className='startBtn'>Take A Test</button>
+             </div>
+            </div>
+            <div className="hero-pic">
+            <img src={hero} alt="hero pic" />
+            </div>
+          
+          
+        </div>
+    );
+};
+
+export default Hero;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css'
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
+import Hero from '../Hero/Hero';
 
 const Home = () => {
      const loadTopic = useLoaderData().data
@@ -9,17 +10,28 @@ const Home = () => {
      
     return (
         <div>
-            <h2>this is HomePage</h2>
+           {/* hero section */}
+
+           <div className="heroSection">
+            <Hero/>
+           </div>
 
             {/* carts */}
-            <div className='carts'>
-                {
-                  loadTopic.map(topic=><Cart
-                    key={topic.id}
-                    topic={topic}
-                  />)  
-                }
-            </div>
+           <div className="topic-info">
+           <div className="topic-title">
+           <h2>Choose your Favourite Topic</h2>
+              <h3 className='color-gray'>Take A tour</h3>
+           </div>
+           <div className='carts'>
+              
+              {
+                loadTopic.map(topic=><Cart
+                  key={topic.id}
+                  topic={topic}
+                />)  
+              }
+          </div>
+           </div>
 
         </div>
     );
